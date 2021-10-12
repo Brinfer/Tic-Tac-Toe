b383090f1929b0e3d4d4d495f305773e3ea88cad
frissada <frissant.damien@gmail.com>
2021-10-12 14:41:18 +0200

    [BUG] Write just into the first row

--- src/game.rs
+++ src/game.rs
@@ -5,65 +5,97 @@ fn main() {
 }
 
 fn start_game() {
-    let size = 3 * 3;
-    let mut numbers: Vec<String> = Vec::new();
+    let size = 3;
+    let mut vec: Vec<Vec<String>> = vec![vec![0.to_string(); size]; size];
+    let mut value: i32 = 0;
 
-    for i in 1..(size + 1) {
-        numbers.push(i.to_string());
-        println!("{:?}", numbers);
+    for i in 0..size {
+        print!("+");
+
+        let mut x = 0;
+        while x < size {
+            print!("---+");
+            x += 1;
+        }
+
+        println!();
+        print!("|");
+
+        for j in 0..size {
+            vec[i][j] = value.to_string();
+            value += 1;
+            if (value) > 10 {
+                print!("{} |", vec[i][j]);
+            } else {
+                print!(" {} |", vec[i][j]);
+            }
+        }
+        println!();
     }
+    let mut x = 0;
+    print!("+");
+    while x < size {
+        print!("---+");
+        x += 1;
+    }
+    println!();
 
-    println!("\x1B[2J\x1B[1;1H");
-    //TODO auto generate the grid according to the size
-    println!(
-        " Grille d'initialisation:\n
-+---+---+---+
-| {} | {} | {} |
-+---+---+---+
-| {} | {} | {} |
-+---+---+---+
-| {} | {} | {} |
-+---+---+---+
-\nEntrer le nombre où vous souhaitez placer votre symbole\n",
-        numbers[0],
-        numbers[1],
-        numbers[2],
-        numbers[3],
-        numbers[4],
-        numbers[5],
-        numbers[6],
-        numbers[7],
-        numbers[8]
-    );
+    /*for i in 0..size{
+
+    }*/
 
     let mut i = 0;
-    let mut player: bool = true;
-    while i < size {
+
+    let to_flatted: Vec<Vec<String>> = vec.clone();
+
+    //Convert 2D matrix in 1D matrix
+    let flattened = to_flatted.into_iter().flatten().collect::<Vec<String>>();
+    let mut player = true;
+
+    while i < (size * size) {
         let line = &*get_keypad();
-        //Look into numbers and return a bool if the variable 'line' is in
-        let present = numbers.iter().any(|x| x == line);
-        //println!("Est present ? {}", present);
+        let convert: u8 = line.parse().unwrap();
 
-        if present {
-            let index = numbers.iter().position(|r| r == line).unwrap();
-            //println!("Le {:?} se trouve en {:?} position", line, index);
-
-            if player {
-                numbers[index] = "X".to_string();
-                player = false;
+        if convert as usize > (size * size - 1) {
+            println!("Error, the matrix is too big, please choose another number");
+        } else {
+            //Get if the entering number is in "flattened"
+            let present = flattened.iter().any(|x| x.to_string() == line.to_string());
+            //Get the row by divided the position of the flat matrix by the nb of row (size)
+            let row_index = (flattened.iter().position(|r| r == line).unwrap()) / size;
+            println!("{}", row_index);
+            let line_index: usize = 0;
+            if present {
+                //Get the line index of the number
+                let line_index = vec[row_index]
+                    .iter()
+                    .position(|r| r.to_string() == line.to_string())
+                    .unwrap();
+                println!("Value 2 present at: [{}][{}]", row_index, line_index);
+                println!("Valeur de vec: {}", vec[row_index][line_index]);
             } else {
-                numbers[index] = "O".to_string();
-                player = true;
+                println!("This cell is already used, please choose another one");
             }
-            println!("\x1B[2J\x1B[1;1H");
-            print_new_grid(&numbers);
-            if check_win(&numbers) {
+            if player {
+                vec[row_index][line_index] = "X".to_string();
+                player = false;
+                println!("Valeur de vec: {}", vec[row_index][line_index]);
+            } else {
+                vec[row_index][line_index] = "O".to_string();
+                player = true;
+                println!("Valeur de vec: {}", vec[row_index][line_index]);
+            }
+            //println!("\x1B[2J\x1B[1;1H");
+            print_new_grid(& mut vec, &size);
+            
+            /*if check_win(&vec) {
                 println!("You win !");
                 return;
-            }
+            }*/
+
             i += 1;
-        } else {
-            println!("This cell is already used, please choose another one");
+            println!("Valeur en 2;2 : {}", vec[1][1].to_string());
+            print!("Valeur en 2;2 : {}", vec[2][2].to_string());
         }
     }
 }
@@ -78,27 +110,31 @@ fn get_keypad() -> String {
     key_entry.replace("\n", "").replace("\r", "")
 }
 
-fn print_new_grid(numbers: &Vec<String>) {
-    println!(
-        "
-+---+---+---+
-| {} | {} | {} |
-+---+---+---+
-| {} | {} | {} |
-+---+---+---+
-| {} | {} | {} |
-+---+---+---+
-",
-        numbers[0],
-        numbers[1],
-        numbers[2],
-        numbers[3],
-        numbers[4],
-        numbers[5],
-        numbers[6],
-        numbers[7],
-        numbers[8]
-    );
+fn print_new_grid(vec: & mut Vec<Vec<String>>, size: &usize) {
+    for i in 0..*size {
+        print!("+");
+
+        let mut x = 0;
+        while x < *size {
+            print!("---+");
+            x += 1;
+        }
+
+        println!();
+        print!("|");
+
+        for j in 0..*size {
+            print!(" {} |", vec[i][j]);
+        }
+        println!();
+    }
+    let mut x = 0;
+    print!("+");
+    while x < *size {
+        print!("---+");
+        x += 1;
+    }
+    println!();
 }
 
 fn check_win(numbers: &Vec<String>) -> bool {
