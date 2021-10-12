961f98385d3ae027246be71a92298c88de492435
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-10-12 17:53:27 +0200

    [FEAT] Improve the game gestion

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,105 @@
-///Author: Damien Frissant
+//! Module allowing all the management of the tic-tac-toe grid.
+//!
+//! # Author
+//! Pierre-Louis GAUTIER
+//! Damien FRISSANT
+
 use std::io::stdin;
-fn main() {
-    start_game();
+
+const OPPONENT_SYMBOL: &str = "O";
+const PLAYER_SYMBOL: &str = "X";
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                              Public
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn init_grid() -> Vec<Vec<String>> {
+    let mut l_grid = vec![vec![0.to_string(); 4]; 4];
+
+    for i in 0..l_grid.len() {
+        for j in 0..l_grid[i].len() {
+            l_grid[i][j] = ((4 * i) + j).to_string();
+        }
+    }
+    return l_grid;
+}
+
+pub fn change_cell(p_grid: &Vec<Vec<String>>, p_cell: u8, p_symbol: String) {
+    // It's ok
+    println!("Row {}", (p_cell as usize) / p_grid.len());
+    println!("Column {}", (p_cell as usize) % p_grid.len());
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                              Private
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fn test_winner(p_grid: &Vec<Vec<String>>) {
+    let mut l_counter: i8 = 0;
+
+    // Test in the row
+    for i in 0..p_grid.len() {
+        for j in 0..p_grid[i].len() {
+            if p_grid[i][j] == OPPONENT_SYMBOL {
+                l_counter -= 1;
+            } else if p_grid[i][j] == PLAYER_SYMBOL {
+                l_counter += 1;
+            }
+        }
+    }
+
+    if (l_counter >= 3) || (l_counter <= -3) {
+        println!("There is a winner");
+    }
+
+    // Test in the column
+    for i in 0..p_grid.len() {
+        l_counter = 0;
+        for j in 0..p_grid[i].len() {
+            if p_grid[i][j] == OPPONENT_SYMBOL {
+                l_counter -= 1;
+            } else if p_grid[i][j] == PLAYER_SYMBOL {
+                l_counter += 1;
+            }
+        }
+        if (l_counter >= 3) || (l_counter <= -3) {
+            println!("There is a winner");
+        }
+    }
+
+    l_counter = 0;
+
+    // Check Left-to-Right downward Diagonal:
+    for i in 0..p_grid.len() {
+        if p_grid[i][i] == OPPONENT_SYMBOL {
+            l_counter -= 1;
+        } else if p_grid[i][i] == PLAYER_SYMBOL {
+            l_counter += 1;
+        }
+    }
+
+    if (l_counter >= 3) || (l_counter <= -3) {
+        println!("There is a winner");
+    }
+
+    // Check Left-to-Right upward Diagonal
+    l_counter = 0;
+    for i in 0..p_grid.len() {
+        if p_grid[i][(p_grid[i].len() - 1) - i] == OPPONENT_SYMBOL {
+            l_counter -= 1;
+        } else if p_grid[i][(p_grid[i].len() - 1) - i] == PLAYER_SYMBOL {
+            l_counter += 1;
+        }
+    }
+
+    if (l_counter >= 3) || (l_counter <= -3) {
+        println!("There is a winner");
+    }
 }
 
 fn start_game() {
